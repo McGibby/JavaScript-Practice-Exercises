be654e2841a289231087e10b296785ac2e4810e3
Michelle <holly8118@gmail.com>
2016-11-07 18:40:36 +1100

    Add files via upload

---
+++ Guess-the-number.js
@@ -0,0 +1,52 @@
+//Guess the number between 1-10 with number set at random
+
+var number = Math.floor((Math.random() * 10) + 1);
+var guess = prompt("Guess the number between 1 and 10");
+
+function guesses(){
+if (guess==number){
+    	 alert("You are correct");
+         guess = false;}
+   else if (guess>number){
+  	 alert("Lower");
+      guess = prompt("Guess again");}
+   else if (guess<number){
+  	 alert("Higher");
+     guess =prompt("Guess again");}
+}
+
+while(guess){
+    guesses();
+}
+
+
+//Guess the number between 1-100 with number preset
+var number = 4;
+var guess = prompt("Guess the number between 1 and 10");
+
+function guesses(){
+if (guess==number){
+    	 alert("You are correct");
+         guess = false;}
+   else if (guess>number){
+  	 alert("Lower");
+      guess = prompt("Guess again");}
+   else if (guess<number){
+  	 alert("Higher");
+     guess =prompt("Guess again");}
+}
+
+while(guess){
+    guesses();
+}
+
+//Guess the number between 1-100 with number preset and no loop
+var number=24
+var guess = prompt("Guess a number between 1 and 100");
+
+if(guess==number){
+	console.log("Correct");}
+else if (guess<number){
+	console.log("Higher");}
+else{
+	console.log("Lower");}
