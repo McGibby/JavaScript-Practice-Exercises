bee970d04197d220377f8d50dd6da3dbc4907812
Michelle <holly8118@gmail.com>
2016-11-08 18:09:28 +1100

    Update Dulpulicate-Array-Remover.js

--- Dulpulicate-Array-Remover.js
+++ Dulpulicate-Array-Remover.js
@@ -1,7 +1,5 @@
 /*Checks two arrays and removes dulicate items
-
 Write a function removeVowels that takes an array of all the letters in the alphabet, and returns an array that removes all the vowels.
-
 Running your function by passing in alphabet should give you an array of all consonants.*/
 
 var vowels = ['a','e','i','o','u'];
