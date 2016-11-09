f8423f3cc4567d0aa3836d3850aa9d7e84c77819
Michelle <holly8118@gmail.com>
2016-11-10 06:33:46 +1100

    Update Minus-max-and-min.js

--- Minus-max-and-min.js
+++ Minus-max-and-min.js
@@ -1,11 +1,13 @@
+//removes the smallest and largest numbers of an array and then returns the sum of the remaining numbers
+
 array = [1,5,3,5,9];
 
 function sumArray(array) {
-  array.sort();
-  array.shift();
-  array.pop();
+  array.sort(); //sort in lowest to highest
+  array.shift(); //remove first number
+  array.pop(); //remove last number
   var sum = array.reduce(function(a,b){
-	return a+b;},0);
+	return a+b;},0); //reduce remaining numbers to single number
   console.log(sum);
 };
 
